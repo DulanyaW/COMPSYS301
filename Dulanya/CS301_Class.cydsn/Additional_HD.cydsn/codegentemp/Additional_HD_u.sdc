22a427821ab58287cf4fd80daabcae022f1639f4
DulanyaW <dwit264@aucklanduni.ac.nz>
2023-08-20 13:31:45 +1200

    Led program

---
+++ Dulanya/CS301_Class.cydsn/Additional_HD.cydsn/codegentemp/Additional_HD_u.sdc
@@ -0,0 +1,3 @@
+# Component constraints for C:\Users\dulan\OneDrive\Desktop\ENGGEN\2023_sem2\301\COMPSYS301\Dulanya\CS301_Class.cydsn\Additional_HD.cydsn\TopDesign\TopDesign.cysch
+# Project: C:\Users\dulan\OneDrive\Desktop\ENGGEN\2023_sem2\301\COMPSYS301\Dulanya\CS301_Class.cydsn\Additional_HD.cydsn\Additional_HD.cyprj
+# Date: Fri, 18 Aug 2023 05:31:26 GMT
---
+++ Dulanya/CS301_Class.cydsn/Additional_HD.cydsn/main.c
@@ -0,0 +1,63 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+/* ========================================
+ * Fully working code: 
+ * PWM      : 
+ * Encoder  : 
+ * ADC      :
+ * USB      : port displays speed and position.
+ * CMD: "PW xx"
+ * Copyright Univ of Auckland, 2016
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF Univ of Auckland.
+ *
+ * ========================================
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <project.h>
+
+
+#include "project.h"
+
+CY_ISR(isr_3_interrupt) {
+//    if(Comp_1_GetCompare()==1){
+//        LED_Write(1);
+//    }
+//    else{
+//        LED_Write(0);
+//    }
+    Comp_1_Init();
+
+}
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    Timer_2_Start();
+    Timer_2_SetInterruptMode(0x02);
+    isr_3_Start();
+    Comp_1_Start();
+    
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
