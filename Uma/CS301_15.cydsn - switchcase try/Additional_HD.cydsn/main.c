2b0d26e6ead5695cfa9b4e5a0992af4057650ac4
ukul426 <ukul426@aucklanduni.ac.nz>
2023-09-29 17:49:43 +1300

    v vv

--- Uma/CS301_15.cydsn - switchcase try/Additional_HD.cydsn/main.c
+++ Uma/CS301_15.cydsn - switchcase try/Additional_HD.cydsn/main.c
@@ -13,11 +13,26 @@ typedef enum {
     GO_STRAIGHT,
     TURN_LEFT,
     TURN_RIGHT,
-    RIGHT_ADJUST,
-    LEFT_ADJUST,
+
     STOP,
 } RobotState;
+//distance calculation paras
+int32 encoderCounts_M1 = 0;  
+int32 encoderCounts_M2 = 0;  
+int32 CPR = 228;  // Adjusted for 4x resolution(228/4)
+float wheelCircumference_cm = (M_PI*64.46)/10;// wheel circumference wheelDiameter_mm = 64.46)
+double timeInterval_s = 10.924;  // Effective time interval( (timer period )2.731*4)
+float32  distance_M1 = 0;
+float32  distance_M2 = 0;
+float32  current_distance_M1 = 0;
+float32  current_distance_M2 = 0;
+float32 target_diatance = 0;//cm
+float32 turn_back_diatance = 10.125;//cm
 
+float32 speed_M1 = 0;
+float32 speed_M2 = 0;
+int32 encoder_value_sum_M1 = 0;
+int32 encoder_value_sum_M2 = 0;
 uint32 count = 0;
 uint8 comp0_sum;
 uint8 comp1_sum;
@@ -30,8 +45,13 @@ bool s1_flag;
 bool s2_flag;
 bool s3_flag;
 
-RobotState current_state = GO_STRAIGHT;// intialse state
+bool is_turning;
+uint8 counter = 1;
+bool is_turning = false; // Initialize is_turning to false
+bool continue_turn = false; // Added a flag to continue the turn
 
+RobotState current_state = GO_STRAIGHT;// intialse state
+//
 //CY_ISR(isr_2_handler) {
 void sensor_status(){
     if (comp0_sum==0 ) {
@@ -49,7 +69,29 @@ void sensor_status(){
     }    
 }
 
+CY_ISR(isr_3_handler) {
+    if (counter < 10){
+        counter++;
+    }else{
+        counter = 1;
+        // Encoder counts (negative due to counterclockwise rotation)
+        encoderCounts_M1 = abs(QuadDec_M1_GetCounter());//QuadDec_M1_GetCounter();
+        encoderCounts_M2 = abs(QuadDec_M2_GetCounter());
+        
+        // sum of encodercounts
+        encoder_value_sum_M1 += encoderCounts_M1;
+        encoder_value_sum_M2 += encoderCounts_M2;
+        
+        // distance calculations 
+        distance_M1 = (encoder_value_sum_M1/CPR) * wheelCircumference_cm;
+        distance_M2 = (encoder_value_sum_M2/CPR) * wheelCircumference_cm;
 
+        //reset the encoder counters 
+        QuadDec_M1_SetCounter(0);
+        QuadDec_M2_SetCounter(0);     
+    }
+        Timer_1_ReadStatusRegister();
+}
 CY_ISR(isr_1_handler) {
     //every 1ms   
     //sum up comp values every 1ms
@@ -99,63 +141,68 @@ int main(void)
     
     QuadDec_M1_Start();
     QuadDec_M2_Start();
-
+    
     
     for(;;)
     {      
         sensor_status();
-            if(comp0_sum==0 || comp1_sum==0){
+            if(s0_flag || s1_flag){
                     current_state = GO_STRAIGHT;
-            }
-            if(s2_flag) {
+            }else if(s2_flag) {
+                    distance_M1 = 0;
                     QuadDec_M1_SetCounter(0);
+                    is_turning = true;
                     current_state = TURN_LEFT;
-            }
-            if(s3_flag){
+            }else if(s3_flag){
                     QuadDec_M2_SetCounter(0);
                     current_state = TURN_RIGHT;
-            }
-            if(comp0_sum>0 && comp1_sum>0 && comp2_sum>0 && comp3_sum>0){
+            }else if(comp0_sum>0 && comp1_sum>0 && comp2_sum>0 && comp3_sum>0){
                     current_state = STOP;
             }
-            if(comp0_sum>0 && comp1_sum==0){//s_ML out of line
-                    current_state = LEFT_ADJUST;
-            }
-            if(comp0_sum==0 && comp1_sum>0){//s_MR out of line
-                    current_state = RIGHT_ADJUST;
-            }
+            
+            
+            
+            
+            
     
             
             switch (current_state) {
             case GO_STRAIGHT:
-                PWM_1_WriteCompare(69);
-                PWM_2_WriteCompare(70);
-                    break;
-            case TURN_LEFT:
-                if(abs(QuadDec_M1_GetCounter()) < TURN_90_ANGLE_EN_COUNT){
+                if(comp0_sum>0 && comp1_sum==0){//s_ML out of line
+                     PWM_L = PWM_2_ReadCompare();
+                     PWM_2_WriteCompare(PWM_L+1); 
+                }else if(comp0_sum==0 && comp1_sum>0){//s_MR out of line
+                        PWM_R = PWM_1_ReadCompare();
+                        PWM_1_WriteCompare(PWM_R+1);
+                }else {
                     PWM_1_WriteCompare(70);
-                    PWM_2_WriteCompare(30);
+                    PWM_2_WriteCompare(70); 
                 }
-                 s2_flag = false;
-                current_state = GO_STRAIGHT;
+                s0_flag = false;
+                s1_flag = false;
+                break;
+                
+            case TURN_LEFT:
+                if(distance_M1 < 3){
+                    PWM_1_WriteCompare(80);
+                    PWM_2_WriteCompare(40); 
+                }else{  
                     
+                    current_state = GO_STRAIGHT;   
+                }   
+                s2_flag= false;
                 break;   
+                
+                
             case TURN_RIGHT:
-                if(abs(QuadDec_M2_GetCounter()) < TURN_90_ANGLE_EN_COUNT){
-                    PWM_1_WriteCompare(30);
-                    PWM_2_WriteCompare(70);
-                    }
+                if(abs(QuadDec_M2_GetCounter()) < 80){
+                    PWM_1_WriteCompare(40);
+                    PWM_2_WriteCompare(80);
+                }else{
+                    current_state = GO_STRAIGHT;  
+                }
                 s3_flag = false;
-                current_state = GO_STRAIGHT;
                 break;  
-            case RIGHT_ADJUST:
-//                PWM_R = PWM_1_ReadCompare();
-//                PWM_1_WriteCompare(PWM_R+1);
-                break; 
-            case LEFT_ADJUST:
-//                PWM_L = PWM_2_ReadCompare();
-//                PWM_2_WriteCompare(PWM_L+1);            
-                break; 
             case STOP:
                 PWM_1_WriteCompare(50);
                 PWM_2_WriteCompare(50);
